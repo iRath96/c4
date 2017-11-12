7c6d428a78ab873eea1c203ed28e87a1cc538f88
Alexander Rath <s8alrath@stud.uni-saarland.de>
2017-11-12 22:08:02 +0100

    parser: enhance error reporting for struct declarations

--- src/Parser.h
+++ src/Parser.h
@@ -518,7 +518,7 @@ protected:
     
     bool read_struct_declaration(ast::Declaration &node)
     OPTION
-        NON_OPTIONAL(read_specifier_qualifier_list(node.specifiers))
+        BEGIN_UNIQUE(read_specifier_qualifier_list(node.specifiers))
         OPTIONAL(read_struct_declarator_list(node.declarators))
         NON_OPTIONAL(read_punctuator(Token::Punctuator::SEMICOLON))
     END_OPTION
---
+++ tests/parser/incomplete_struct_declaration/input.c
@@ -0,0 +1,1 @@
+struct { int; struct { int; } } x;
