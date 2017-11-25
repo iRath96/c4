db687715f1606360b606bd0a14adaecda5fec932
Alexander Rath <s8alrath@stud.uni-saarland.de>
2017-11-25 18:10:38 +0100

    test: add more beautifier tests

---
+++ tests/ast/declarators/input.c
@@ -0,0 +1,4 @@
+int main(int);
+int (main)(int);
+int (((main(int))));
+int main;
---
+++ tests/ast/if/input.c
@@ -0,0 +1,13 @@
+int main() {
+    if ((4)) {
+        if ((4))
+            if (2)
+                goto end;
+        
+        if (2)
+            goto begin;
+        
+        // madness!
+    } else
+        while (1+1);
+}
