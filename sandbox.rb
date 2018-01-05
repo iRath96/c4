d6a7850b5976d9614ef6afd540a1a7a063864bd3
Alexander Rath <s8alrath@stud.uni-saarland.de>
2018-01-05 13:27:02 +0100

    chore: add sandbox

---
+++ sandbox.rb
@@ -0,0 +1,17 @@
+require "open3"
+require "diffy"
+
+$BIN = "#{__dir__}/build/debug/c4"
+
+Dir.mkdir(".test") rescue nil
+Dir.chdir(".test")
+test = "../#{test}"
+
+arguments = [ $BIN, "/Users/alex/Desktop/test.c" ].compact
+stdout, stderr, status = Open3.capture3(*arguments)
+if status.exitstatus > 0
+  puts stderr
+  exit 1
+end
+
+puts `llc -relocation-model=pic -filetype=obj test.ll; gcc -fPIC test.o -o test`
