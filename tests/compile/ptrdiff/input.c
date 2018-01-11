02f937b43e378bc45e25ebbd73caf495807bdbe9
Alexander Rath <s8alrath@stud.uni-saarland.de>
2018-01-11 08:30:10 +0100

    test: add compile/ptrdiff test

---
+++ tests/compile/ptrdiff/input.c
@@ -0,0 +1,21 @@
+int printf(char *, ...);
+void *malloc(int);
+
+int main(void) {
+	int n = 4;
+	int *a = malloc(sizeof(int) * n);
+	int *b = a + n;
+	int *i = a;
+
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 4;
+
+	while (i < b) {
+		printf("%d: %d\n", i - a, *i);
+		++i;
+	}
+
+	printf("last: *%x = %d\n", b - 1, *(b - 1));
+}
